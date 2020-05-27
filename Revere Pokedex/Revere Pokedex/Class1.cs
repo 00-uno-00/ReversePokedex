ed8dbe0c355eb3777b94f06a0998b3975e9d0dee
00-uno-00 <59146921+00-uno-00@users.noreply.github.com>
2020-05-27 15:22:47 +0200

    Add files via upload

---
+++ Revere Pokedex/Revere Pokedex/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Pokemondata
+{
+    struct Stats
+    {
+        public int hp;
+        public int atk;
+        public int def;
+        public int spAtk;
+        public int spDef;
+        public int spd;
+    }
+    struct Pokemon
+    {
+        public int id;
+        public char name;
+        public char type1;
+        public char type2;
+        public Stats stats;
+        public int gen;
+        public char legg;
+        public 
+int read;
+    }
+}
+
---
+++ Revere Pokedex/Revere Pokedex/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pokemondata;
+using System.Windows.Forms;
+
+namespace Revere_Pokedex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (System.IO.StreamReader sr = new System.IO.StreamReader("pokemon_data_2.txt"))
+                {
+                    string line;
+                    // Read and display lines from the file until the end of
+                    // the file is reached.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        MessageBox.Show(line);
+                    }
+                }
+            }
+            catch 
+            {
+                MessageBox.Show("errore");
+            }
+        }
+    }
+}
